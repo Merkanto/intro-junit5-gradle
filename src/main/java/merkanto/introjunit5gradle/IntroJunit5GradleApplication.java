f37978caa9c77f1bab175751ee2aba0d0060a25a
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-03 07:29:19 +0200

    Init commit.

---
+++ src/main/java/merkanto/introjunit5gradle/IntroJunit5GradleApplication.java
@@ -0,0 +1,13 @@
+package merkanto.introjunit5gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroJunit5GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroJunit5GradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/merkanto/introjunit5gradle/IntroJunit5GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package merkanto.introjunit5gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroJunit5GradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
